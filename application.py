3ab58270e5310937b2318dba34f8694b295ea3af
laneboi <solid.formalin@gmail.com>
2018-11-18 13:03:22 +0300

    Added registration form

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -25,3 +25,7 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/")
 def index():
     return "Project 1: TODO"
+
+@app.route('/registration')
+def register():
+    return render_template('registration.html')
