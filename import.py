80369d0187eaf24b1f80d3569f836527c1ad0eef
laneboi <solid.formalin@gmail.com>
2018-11-21 00:44:15 +0300

    Imported data to the db

--- import.py
+++ import.py
@@ -2,10 +2,15 @@ import csv
 from application import db
 
 
-with open('books.csv') as csvfile:
-	csvreader = csv.reader(csvfile, delimiter=',')
-	for row in csvreader:
-		if db.execute("SELECT isbn FROM books WHERE isbn = :isbn", {'isbn': row[0]}).rowcount == 0:
-			db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
-					   {'isbn': row[0], 'title': row[1], 'author': row[2], 'year': row[3]})
-	db.commit()
+def main():
+	with open('books.csv') as csvfile:
+		csvreader = csv.DictReader(csvfile, delimiter=',')
+		for row in csvreader:
+			if db.execute("SELECT isbn FROM books WHERE isbn = :isbn", {'isbn': row['isbn']}).rowcount == 0:
+				db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+						   {'isbn': row['isbn'], 'title': row['title'], 'author': row['author'], 'year': row['year']})
+		db.commit()
+
+
+if __name__ == '__main__':
+	main()
